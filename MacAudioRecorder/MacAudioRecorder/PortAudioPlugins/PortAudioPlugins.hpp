31625d24ac58d499e365e8708282dcc90e2f732b
GuanSuns <guansuns@outlook.com>
2019-04-04 16:57:05 -0500

    disable Stop button

--- MacAudioRecorder/MacAudioRecorder/PortAudioPlugins/PortAudioPlugins.hpp
+++ MacAudioRecorder/MacAudioRecorder/PortAudioPlugins/PortAudioPlugins.hpp
@@ -16,23 +16,23 @@
 #define SAMPLE_RATE  (48000)
 #define FRAMES_PER_BUFFER (512)
 #define NUM_SECONDS     (20)
-#define NUM_CHANNELS    (2)
+#define NUM_CHANNELS    (1)
 /* #define DITHER_FLAG     (paDitherOff) */
 #define DITHER_FLAG     (0) /**/
 /** Set to 1 if you want to capture the recording to a file. */
 #define WRITE_TO_FILE   (1)
 
 /* Select sample format. */
-#if 0
-#define PA_SAMPLE_TYPE  paFloat32
-typedef float SAMPLE;
-#define SAMPLE_SILENCE  (0.0f)
-#define PRINTF_S_FORMAT "%.8f"
-#elif 1
+#if 1
 #define PA_SAMPLE_TYPE  paInt16
 typedef short SAMPLE;
 #define SAMPLE_SILENCE  (0)
 #define PRINTF_S_FORMAT "%d"
+#elif 0
+#define PA_SAMPLE_TYPE  paFloat32
+typedef float SAMPLE;
+#define SAMPLE_SILENCE  (0.0f)
+#define PRINTF_S_FORMAT "%.8f"
 #elif 0
 #define PA_SAMPLE_TYPE  paInt8
 typedef char SAMPLE;
