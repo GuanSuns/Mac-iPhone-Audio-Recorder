5b150c46bee02f19ec7a755d23067d1c24dbe347
GuanSuns <guansuns@outlook.com>
2019-05-02 09:51:25 -0500

    add wav file parser

---
+++ MacAudioRecorder/MacAudioRecorder/IODevicesConfigurator/IODeviceConfigurator.h
@@ -0,0 +1,15 @@
+//
+//  IODeviceConfigurator.h
+//  MacAudioRecorder
+//
+//  Created by Lin Guan on 5/1/19.
+//  Copyright © 2019 Lin Guan. All rights reserved.
+//
+
+#import <portaudio.h>
+
+#ifndef IODeviceConfigurator_h
+#define IODeviceConfigurator_h
+
+
+#endif /* IODeviceConfigurator_h */
---
+++ MacAudioRecorder/MacAudioRecorder/IODevicesConfigurator/IODeviceConfigurator.m
@@ -0,0 +1,9 @@
+//
+//  IODeviceConfigurator.m
+//  MacAudioRecorder
+//
+//  Created by Lin Guan on 5/1/19.
+//  Copyright © 2019 Lin Guan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
